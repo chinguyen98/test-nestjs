b42d5407fc00aafdaff506c03acfa8fdace415cc
chinguyen98 <dacchi6761@gmail.com>
2020-04-25 22:18:41 +0700

    test

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -12,11 +12,29 @@ export class BooksService {
     getBook(bookId): Promise<any> {
         let id = Number(bookId);
         return new Promise(resolve => {
-            const book = this.books.find(book => book.id = id);
+            const book = this.books.find(book => book.id === id);
             if (!book) {
                 throw new HttpException('Book does not exist!', 404);
             }
             resolve(book);
+        });
+    }
+
+    storeBook(book): Promise<any> {
+        return new Promise(resolve => {
+            this.books.push(book);
+            resolve(book);
+        });
+    }
+
+    deleteBook(bookId): Promise<any> {
+        let id = Number(bookId);
+        return new Promise(resolve => {
+            let index = this.books.findIndex(book => book.id === id);
+            if (index === -1) {
+                throw new HttpException('Book does not exist!', 404);
+            }
+            this.books.splice(index, 1);
         })
     }
 }
